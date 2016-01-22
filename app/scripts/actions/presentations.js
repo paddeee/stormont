fa28b3b798c3c8fcddaa59f0f9a09e28ead9fe51
paddeee <paddeee@googlemail.com>
2016-01-22 15:13:45 +0000

    Fixed bug with Undefined transforms being ste on import

--- app/scripts/actions/presentations.js
+++ app/scripts/actions/presentations.js
@@ -4,6 +4,5 @@ var Reflux = require('reflux');
 
 module.exports = Reflux.createActions([
   'presentationStateChanged',
-  'approvalStateChanged',
-  'exportPresentation'
+  'approvalStateChanged'
 ]);
