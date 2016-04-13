ea06ee3b7a2e85129df1bcec1d79a60af1042f29
paddeee <paddeee@googlemail.com>
2016-04-13 17:44:02 +0100

    Added fix for when events are cleared from timeline then added back with unique_id

--- app/scripts/vendor/timeline/timeline-overrides.js
+++ app/scripts/vendor/timeline/timeline-overrides.js
@@ -2,3 +2,9 @@
 
 TL.ajax = function() {};
 TL.getJSON = function() {};
+
+// Override unique key functionality as it causes issues when we have to leave a last event in in the timeline when
+// clearing the timeLine.
+TL.Util.ensureUniqueKey = function(obj, candidate) {
+  return candidate;
+};
