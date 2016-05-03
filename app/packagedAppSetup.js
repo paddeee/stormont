c1bad3084b124171702170054f0bbd86f45c0da6
paddeee <paddeee@googlemail.com>
2016-05-03 19:05:30 +0100

    Added offline mapping

--- app/packagedAppSetup.js
+++ app/packagedAppSetup.js
@@ -7,7 +7,23 @@ if (typeof process === 'object') {
 
   remote = require('electron').remote;
 
-  config = JSON.parse(remote.getGlobal('config'));
+  config = remote.getGlobal('config');
+
+  // preserve newlines, etc - use valid JSON
+  config = config.replace(/\\n/g, "\\n")
+    .replace(/\\'/g, "\\'")
+    .replace(/\\"/g, '\\"')
+    .replace(/\\&/g, "\\&")
+    .replace(/\\r/g, "\\r")
+    .replace(/\\t/g, "\\t")
+    .replace(/\\b/g, "\\b")
+    .replace(/\\f/g, "\\f");
+
+  // remove non-printable and other non-valid JSON chars
+  config = config.replace(/[\u0000-\u0019]+/g,"");
+
+  // Conver to JavaScript Object
+  config = JSON.parse(config);
 
   // Use for non browserify requires
   electronRequire = require;
@@ -15,3 +31,5 @@ if (typeof process === 'object') {
   // Use to communicate with Electron
   var ipcRenderer = require('ipc');
 }
+
+
