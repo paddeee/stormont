e38c3f56411e27fd1fd6c2f8b3149aba507579cd
paddeee <paddeee@googlemail.com>
2016-09-05 11:36:29 +0100

    Publish as Evidence Functionality not working #48

--- app/scripts/stores/screen.js
+++ app/scripts/stores/screen.js
@@ -29,7 +29,7 @@ module.exports = Reflux.createStore({
     this.trigger(this);
 
     controllerWindow = windows.find(function (window) {
-      return window.getTitle() === 'EPE Electronic Presentation of Evidence';
+      return window.getTitle() === 'Electronic Presentation of Evidence';
     });
 
     rect = this.getPageCrop(publishObject, controllerWindow);
